b7dc1931705fea7c6bd12ef0fd37353b83778ee0
Sinan <sinandeemir@gmail.com>
2024-03-05 21:43:04 +0300

    first commit

---
+++ java/ders1-odev/src/main/loopDemo/LoopDemo.java
@@ -0,0 +1,25 @@
+public class LoopDemo {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i += 2) {
+      System.out.println(i);
+    }
+
+    System.out.println("For döngüsü bitti.");
+
+    int i = 1;
+    while (i < 10) {
+      System.out.println(i);
+      i += 2;
+    }
+
+    System.out.println("While döngüsü bitti.");
+
+    int j = 100;
+    do {
+      System.out.println(j);
+      j += 2;
+    } while (j < 10);
+    System.out.println("Do-While döngüsü Bitti");
+
+  }
+}
